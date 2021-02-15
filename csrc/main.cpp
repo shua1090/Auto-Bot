29ee0e1f2a0e5936cf6ca4edb230bb45483fffb8
Aditya Matam <aditya0310@outlook.com>
2021-02-15 13:16:27 -0800

    Removed extraneous code in motor class

--- csrc/main.cpp
+++ csrc/main.cpp
@@ -1,13 +1,22 @@
 #include "pch.hpp"
 #include "helpers.hpp"
-#include "motor.hpp"
+// #include "motor.hpp"
 
 int main()
 {   
     wiringPiSetup();
 
-    Motor left(P11, P13, P15);
-    left.drive();
+    pinMode(P11, OUTPUT);
+    pinMode(P13, OUTPUT);
+    pinMode(P15, OUTPUT);
+
+    softPwmWrite(P11, 75);
+    digitalWrite(P13, HIGH);
+    digitalWrite(P15, LOW);
     delay(1000);
-    left.stop();
+    softPwmWrite(P11, 0);
+
+    // Motor left(P11, P13, P15);
+    // left.drive();
+    // left.stop();
 }
